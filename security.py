a826fb2067414f79059048f34e629673a085f756
bjbesaw <brian@twolakessolutions.com>
2019-07-09 22:39:33 -0400

    BCrypt troubleshooting.

--- security.py
+++ security.py
@@ -4,7 +4,7 @@ from models.user import UserModel
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if bcrypt.checkpw(user.password, user.password):
+    if bcrypt.checkpw(password.encode('utf-8'), password.encode('utf-8')):
         return user
 
     # if user and safe_str_cmp(user.password, password):
